c89f6b3e726237b8f8e98cf7df94e7a729ddedfe
1119170395@qq.com <1119170395@qq.com>
2025-08-16 13:06:58 +0800

    首次提交

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+
+    output_idx = 0
+    point = 203, 293
+    h = 167
+    w = 205
+    move = 216
---
+++ jietu.py
@@ -0,0 +1,29 @@
+from PIL import Image
+import dxcam
+import gc
+from config import Config
+
+
+def main():
+    grab = dxcam.create(output_idx=Config.output_idx, output_color='RGB')
+    img = None
+    while img is None:
+        img = grab.grab()
+    # print(type(img))
+    # print(img.shape)
+    sub_imgs = []
+    x, y = Config.point
+    for _ in range(5):
+        sub_img = img[y:y+Config.h, x:x+Config.w, :]
+        sub_imgs.append(sub_img)
+        x+=Config.move
+
+    for i, sub_img in enumerate(sub_imgs):
+        sub_img = Image.fromarray(sub_img)
+        sub_img.save(f'img_{i}.png')
+
+    del grab
+    gc.collect()  # 强制垃圾回收
+
+if __name__ == '__main__':
+    main()
